54b39377093fd720e9432138145f8410b28c1e3e
Emina <begic.emina92@gmail.com>
2025-07-09 21:30:07 +0200

    prvi

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Kkao je ovo sranje
         </a>
       </header>
     </div>
